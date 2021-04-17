31f8378d99f46d35a6a1cf3542380d54dac85327
nour-12 <59794834+nour-12@users.noreply.github.com>
2021-04-17 03:46:52 -0700

    Update ec_to_pub.c

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -3,16 +3,16 @@
 
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]){
 
-    const EC_GROUP *group, EC_POINT *point;
+    const EC_GROUP *group0, EC_POINT *point0;
 
 
     if(!key || !pub)
         return NULL;
     
-    group = EC_KEY_get_group (key);
-    point = EC_KEY_get_public_key (key);
+    group0 = EC_KEY_get0_group (key);
+    point0 = EC_KEY_get0_public_key (key);
 
-    if(!EC_POINT_point2oct(group, point,POINT_CONVERSION_UNCOMPRESSED, pub, EC_PUB_LEN, NULL))
+    if(!EC_POINT_point2oct(group0, point0,POINT_CONVERSION_UNCOMPRESSED, pub, EC_PUB_LEN, NULL))
         return NULL;
 
     return pub;
