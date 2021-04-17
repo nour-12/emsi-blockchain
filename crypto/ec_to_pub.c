941a9d8cbf0fbcde74fb8f3d88857b3a07b78ed1
nour-12 <59794834+nour-12@users.noreply.github.com>
2021-04-17 03:43:22 -0700

    Create ec_to_pub.c

---
+++ crypto/ec_to_pub.c
@@ -0,0 +1,20 @@
+#include "hblk_crypto.h"
+
+
+uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]){
+
+    const EC_GROUP *group, EC_POINT *point;
+
+
+    if(!key || !pub)
+        return NULL;
+    
+    group = EC_KEY_get_group (key);
+    point = EC_KEY_get_public_key (key);
+
+    if(!EC_POINT_point2oct(group, point,POINT_CONVERSION_UNCOMPRESSED, pub, EC_PUB_LEN, NULL))
+        return NULL;
+
+    return pub;
+
+}
