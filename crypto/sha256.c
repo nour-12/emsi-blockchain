5ef9f7c42032df8d3e0c033c58c75e08cdc792ff
nour-12 <59794834+nour-12@users.noreply.github.com>
2021-04-18 11:19:27 +0000

    Update sha256.c

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -6,15 +6,13 @@ uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH
 {
     SHA256_CTX sha256;
 
-    if(digest && s)
-    {
+    if(!digest)
+        return NULL;
 
-        SHA256_Init(&sha256);
-        SHA256_Update(&sha256,s,len);
-        SHA256_Final(digest,&sha256);
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256,s,len);
+    SHA256_Final(digest,&sha256);
 
-        return digest;
-    }
-
-    return NULL;
+    return digest;
+    
 }
