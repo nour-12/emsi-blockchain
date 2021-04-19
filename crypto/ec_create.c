ff20d1e45ddb4f79c18a5fbd255edca7eb7692ff
nour-12 <59794834+nour-12@users.noreply.github.com>
2021-04-19 12:55:20 +0000

    Update ec_create.c

--- crypto/ec_create.c
+++ crypto/ec_create.c
@@ -1,18 +1,14 @@
-#include <stdio.h>
 #include "hblk_crypto.h"
-
 EC_KEY *ec_create(void)
 {
-    EC_KEY *pointer;
-
-    pointer = EC_KEY_new(EC_CURVE);
-    if (!pointer) return NULL;
-
-    if (EC_KEY_Kgen(pointer) != 1)
+    EC_KEY * ec_key;
+    ec_key = EC_KEY_new_by_curve_name(EC_CURVE);
+    if (!ec_key)
+        return NULL;
+    if (!EC_KEY_generate_key(ec_key))
     {
-        EC_KEY_free(pointer);
+        EC_KEY_free(ec_key);
         return NULL;
     }
-
-    return pointer;
+    return ec_key;
 }
