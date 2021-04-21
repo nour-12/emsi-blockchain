feb40f63b9da2f8549d8e3497c8fe5d8b0d1e2d4
nour-12 <59794834+nour-12@users.noreply.github.com>
2021-04-21 18:25:02 +0000

    Update sha256.c

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -1,18 +1,9 @@
-#include <stdint.h>
 #include "hblk_crypto.h"
 
-
-uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH])
+uint8_t *sha256(int8_t const *s, size_t len,
+		uint8_t digest[SHA256_DIGEST_LENGTH])
 {
-    SHA256_CTX sha256;
-
-    if(!digest)
-        return NULL;
-
-    SHA256_Init(&sha256);
-    SHA256_Update(&sha256,s,len);
-    SHA256_Final(digest,&sha256);
-
-    return digest;
-    
+	if (!s || !digest)
+		return (NULL);
+	return (SHA256((const unsigned char *)s, len, digest));
 }
