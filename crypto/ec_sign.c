19a4774962e7532d90be8e63b80989fd0084f70a
nour-12 <59794834+nour-12@users.noreply.github.com>
2021-04-24 03:45:57 +0000

    Update ec_sign.c

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -1,28 +1,31 @@
-#include <stdio.h>
 #include "hblk_crypto.h"
 
 /**
- * ec_sign - signs a set of bytes with private key
- * @key: pointer EC_KEY to struct containing key pair
- * @msg: pointer to characters to sign
- * @msglen: length of message
+ * ec_sign - sign a set of bytes, using given private EC_KEY
+ * @key: pointer to EC_KEY struct containing private key to perform the signing
+ * @msg: pointer to characters to be signed
+ * @msglen: len of msg
  * @sig: address to store signature
- * Return: pointer to sig buffer if op succeded or NULL
+ *
+ * Return: pointer to signature buffer on success, NULL on error
  */
 uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen,
-				 sig_t *sig)
+		sig_t *sig)
 {
-	uint32_t len = 0;
+	unsigned char md[SHA256_DIGEST_LENGTH];
 
-	if (!key || !msg || !msglen)
+	if (!key || !msg || !sig)
 		return (NULL);
-
-	memset(sig->sig, 0, sizeof(sig->sig));
-	if (!ECDSA_sign(0, msg, (int)msglen, sig->sig, &len, (EC_KEY *)key))
-	{
-		sig->len = 0;
+	if (!EC_KEY_check_key(key))
 		return (NULL);
-	}
-	sig->len = (uint8_t)len;
+	if (!SHA256(msg, msglen, md))
+		return (NULL);
+	sig->len = ECDSA_size(key);
+	if (!sig->len)
+		return (NULL);
+	if (!ECDSA_sign(EC_CURVE, md, SHA256_DIGEST_LENGTH, sig->sig,
+				(unsigned int *)&(sig->len), (EC_KEY *)key))
+		return (NULL);
 	return (sig->sig);
 }
+
