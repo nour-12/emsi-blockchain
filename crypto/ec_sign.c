b824d12eafe050c5bf18dc00195b63364a7045eb
nour-12 <59794834+nour-12@users.noreply.github.com>
2021-04-24 11:28:41 +0000

    Update ec_sign.c

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -22,7 +22,8 @@ uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen,
 	sig->len = ECDSA_size(key);
 	if(!sig->len)
 		return (NULL);
-	if(!EDCSA_sign(EC_CURVE, hash, SHA256_DIGEST_LENGTH, sig->sig, (unsigned int *)&(sig->len), (EC_KEY *)key))
+	if(!EDCSA_sign(EC_CURVE, hash, SHA256_DIGEST_LENGTH, sig->sig, 
+		       (unsigned int *)&(sig->len), (EC_KEY *)key))
 		return (NULL);
 	return (sig->sig);
 }
