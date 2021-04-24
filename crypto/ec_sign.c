3898227835498a4fdb2ad2086f6d761b499767e3
nour-12 <59794834+nour-12@users.noreply.github.com>
2021-04-24 03:22:55 +0000

    Update ec_sign.c

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -1,24 +1,27 @@
 #include "hblk_crypto.h"
 
 /**
- * ec_sign - signs a message with private key
- * @key: pointer to struct containing key pair
- * @msg: the message to sign
+ * ec_sign - signs a set of bytes with private key
+ * @key: pointer EC_KEY to struct containing key pair
+ * @msg: pointer to characters to sign
  * @msglen: length of message
  * @sig: address to store signature
- * Return: pointer to sig buffer or NULL
+ * Return: pointer to sig buffer if op succeded or NULL
  */
 uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen,
-	sig_t *sig)
+				 sig_t *sig)
 {
-	unsigned int len;
+	uint32_t len = 0;
 
-	if (!key || !msg || !sig)
+	if (!key || !msg || !msglen)
 		return (NULL);
-	len = sig->len;
-	if (ECDSA_sign(0, msg, msglen, sig->sig, &len,
-		(EC_KEY *)key) != 1)
+
+	memset(sig->sig, 0, sizeof(sig->sig));
+	if (!ECDSA_sign(0, msg, (int)msglen, sig->sig, &len, (EC_KEY *)key))
+	{
+		sig->len = 0;
 		return (NULL);
-	sig->len = len;
+	}
+	sig->len = (uint8_t)len;
 	return (sig->sig);
 }
