2c624d83b5ef3cf700dae0f0c785ae0a72fe300b
nour-12 <59794834+nour-12@users.noreply.github.com>
2021-04-24 03:17:50 +0000

    Update ec_to_pub.c

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -1,21 +1,27 @@
-
 #include "hblk_crypto.h"
 
+/**
+ * ec_to_pub - extracting public key from EC_KEY struct
+ * @key: point to EC_KEY structure
+ * @pub: adr of buffer to populate with pub key
+ * Return: adr of populated buffer or NULL
+ */
 
-uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]){
+uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
+{
+const EC_POINT  *publicKey;
+const EC_GROUP  *group;
 
-    //declaration
-    const EC_GROUP *group;
-    const EC_POINT *point;
+if (key == NULL || pub == NULL)
+return (NULL);
 
-   if(key == 0 || pub == 0)
-       return NULL;
 
-    group = EC_KEY_get0_group(key);
-    point = EC_KEY_get0_public_key(key);
-    
-    if(!EC_POINT_point2oct(group, point, POINT_CONVERSION_UNCOMPRESSED, pub, EC_PUB_LEN, NULL))
-        return NULL;
-    return pub;
+publicKey = EC_KEY_get0_public_key(key);
+group = EC_KEY_get0_group(key);
 
+if (!EC_POINT_point2oct(group, publicKey, POINT_CONVERSION_UNCOMPRESSED, pub,
+				EC_PUB_LEN, NULL))
+return (NULL);
+
+return (pub);
 }
