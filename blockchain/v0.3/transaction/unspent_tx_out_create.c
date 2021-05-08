2fbcd7c71fca823ff4249450df0034f7781d0736
Noura <nouraaqqa@Nouras-MBP.Home>
2021-05-08 16:46:40 +0000

    Transaction

--- blockchain/v0.3/transaction/unspent_tx_out_create.c
+++ blockchain/v0.3/transaction/unspent_tx_out_create.c
@@ -8,6 +8,7 @@
  * @out: address of transaction output to be copied
  * Return: new unspent transaction struct
  */
+
 unspent_tx_out_t *unspent_tx_out_create(
 	uint8_t block_hash[SHA256_DIGEST_LENGTH],
 	uint8_t tx_id[SHA256_DIGEST_LENGTH], tx_out_t const *out)
